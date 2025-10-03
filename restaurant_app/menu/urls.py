a092f52b4c8f8d9fa066deb6f102844e4f5eb150
OmarYasser810 <OmarYasser05810@gmail.com>
2025-10-03 18:22:12 +0300

    finish menu app

--- restaurant_app/menu/urls.py
+++ restaurant_app/menu/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', menu_view, name='menu'),
-    path('insert/', insert_menu_items, name='insert_menu_items'),
+    path('', menu, name='menu'),
+    path('insert/', insert_menu_item, name='insert_menu_item'),
+    path('edit/<int:item_id>/', edit_menu_item, name='edit_menu_item'),
+    path('delete/<int:item_id>/', delete_menu_item, name='delete_menu_item'),
 ]
--- restaurant_app/menu/views.py
+++ restaurant_app/menu/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 from .models import MenuItem
 
 # Create your views here.
-def menu_view(request):
+def menu(request):
     menu_items = MenuItem.objects.all()
     return render(request, 'menu.html', {'menu_items': menu_items})
 
-def insert_menu_items(request):
+def insert_menu_item(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         category = request.POST.get('category')
@@ -21,5 +21,23 @@ def insert_menu_items(request):
             price=price,
             photo=photo
         )
-        return redirect('insert_menu_items')
-    return render(request, 'insert_menu_items.html')
+        return redirect('insert_menu_item')
+    return render(request, 'insert_menu_item.html')
+
+def edit_menu_item(request, item_id):
+    item = MenuItem.objects.get(item_id=item_id)
+    if request.method == 'POST':
+        item.name = request.POST.get('name')
+        item.category = request.POST.get('category')
+        item.description = request.POST.get('description')
+        item.price = request.POST.get('price')
+        if 'photo' in request.FILES:
+            item.photo = request.FILES.get('photo')
+        item.save()
+        return redirect('menu')
+    return render(request, 'edit_menu_item.html', {'item': item})
+
+def delete_menu_item(request, item_id):
+    item = MenuItem.objects.get(item_id=item_id)
+    item.delete()
+    return redirect('menu')
