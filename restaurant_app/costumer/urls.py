e535071de109405109a2191070773287046eb2ee
OmarYasser810 <OmarYasser05810@gmail.com>
2025-10-03 20:59:50 +0300

    finish customer

--- restaurant_app/costumer/urls.py
+++ restaurant_app/costumer/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('', customer, name='customer'),
+    path('insert/', insert_customer, name='insert_customer'),
+    path('edit/<int:customer_id>/', edit_customer, name='edit_customer'),
+    path('delete/<int:customer_id>/', delete_customer, name='delete_customer'),
 ]
