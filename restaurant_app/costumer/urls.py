0b3a3cd739fdb07d91f474208347254900f9a85c
Omar Yasser <OmarYasser05810@gmail.com>
2025-10-03 23:50:01 +0300

    Delete restaurant_app directory

--- restaurant_app/costumer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', customer, name='customer'),
-    path('insert/', insert_customer, name='insert_customer'),
-    path('edit/<int:customer_id>/', edit_customer, name='edit_customer'),
-    path('delete/<int:customer_id>/', delete_customer, name='delete_customer'),
-]
